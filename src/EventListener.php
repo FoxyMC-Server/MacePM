a7d5f9dc2bc26a19fb4913f3bd854f20adc004e5
FoxyMC <107026427+FoxyMC-Server@users.noreply.github.com>
2025-11-01 19:58:11 +0100

    Update EventListener.php

--- src/EventListener.php
+++ src/EventListener.php
@@ -34,13 +34,17 @@ use pocketmine\item\enchantment\StringToEnchantmentParser;
 use pocketmine\item\StringToItemParser;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\math\Vector3;
-use pocketmine\network\{mcpe\protocol\PlaySoundPacket};
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\player\Player;
 use pocketmine\world\particle\BlockBreakParticle;
 use XeonCh\Mace\item\Mace;
 
 class EventListener implements Listener
 {
+    private const KNOCKBACK_RANGE = 3.5;
+    private const KNOCKBACK_POWER = 0.7;
+    private const FALL_DISTANCE_THRESHOLD = 1.5;
+    private const HEAVY_SMASH_THRESHOLD = 5.0;
 
     private array $playerFallDistance = [];
 
@@ -60,59 +64,60 @@ class EventListener implements Listener
         }
         if ($player->isOnGround()) {
             if (isset($this->playerFallDistance[$player->getName()])) {
-               /* $fallDistance = $this->playerFallDistance[$player->getName()];
-                if ($fallDistance > 1) {
-                }*/
                 unset($this->playerFallDistance[$player->getName()]);
             }
         }
     }
 
-   public function MaceLogic(EntityDamageByEntityEvent $event): void
-   {
-       $damager = $event->getDamager();
+    public function MaceLogic(EntityDamageByEntityEvent $event): void
+    {
+        $damager = $event->getDamager();
 
-       if (!$damager instanceof Player) {
+        if (!$damager instanceof Player) {
             return;
-       }
-
-       $player = $damager;
-       $item = $player->getInventory()->getItemInHand();
-
-       if (!$item instanceof Mace) {
-           return;
-       }
-
-       $playerName = $player->getName();
-       if (!isset($this->playerFallDistance[$playerName])) {
-           return;
-       }
-
-       $fallDistance = $this->playerFallDistance[$playerName];
-       unset($this->playerFallDistance[$playerName]);
-       
-       if ($fallDistance > 1) {
-            $bonusDamage = floor($fallDistance * 2) - 1;
-            $densityEnchant = $item->getEnchantment(StringToEnchantmentParser::getInstance()->parse("density"));
-            if ($densityEnchant !== null) {
-                $densityLevel = $densityEnchant->getLevel();
-                $densityMultiplier = match ($densityLevel) {
-                    1 => 0.5,
-                    2 => 1.0,
-                    3 => 1.5,
-                    4 => 2.0,
-                    5 => 2.5,
-                    default => 0,
-                };
-
-                $densityBonus = $fallDistance * $densityMultiplier;
-                $bonusDamage += $densityBonus;
-            }
-            $newDamage = $event->getBaseDamage() + $bonusDamage;
-            $event->setBaseDamage($newDamage);
         }
 
-       $target = $event->getEntity();
+        $player = $damager;
+        $item = $player->getInventory()->getItemInHand();
+
+        if (!$item instanceof Mace) {
+            return;
+        }
+
+        $playerName = $player->getName();
+        if (!isset($this->playerFallDistance[$playerName])) {
+            return;
+        }
+
+        $fallDistance = $this->playerFallDistance[$playerName];
+        unset($this->playerFallDistance[$playerName]);
+
+        if ($fallDistance <= self::FALL_DISTANCE_THRESHOLD) {
+            return;
+        }
+
+        $bonusDamage = $this->calculateBonusDamage($fallDistance);
+
+        $densityEnchant = $item->getEnchantment(StringToEnchantmentParser::getInstance()->parse("density"));
+        if ($densityEnchant !== null) {
+            $densityLevel = $densityEnchant->getLevel();
+            $densityMultiplier = match ($densityLevel) {
+                1 => 0.5,
+                2 => 1.0,
+                3 => 1.5,
+                4 => 2.0,
+                5 => 2.5,
+                default => 0,
+            };
+
+            $densityBonus = $fallDistance * $densityMultiplier;
+            $bonusDamage += $densityBonus;
+        }
+
+        $newDamage = $event->getBaseDamage() + $bonusDamage;
+        $event->setBaseDamage($newDamage);
+
+        $target = $event->getEntity();
         if ($target instanceof Player) {
             $enchant = $item->getEnchantment(StringToEnchantmentParser::getInstance()->parse("breach"));
             if ($enchant !== null) {
@@ -137,77 +142,158 @@ class EventListener implements Listener
                 }
             }
         }
-       if ($fallDistance >= 3) {
-           $targetPos = $event->getEntity()->getPosition();
-           $world = $player->getWorld();
-           $blockUnder = $world->getBlock($targetPos->subtract(0, 1, 0));
-           $block = ($blockUnder instanceof Air) ? "grass" : $blockUnder->getName();
-           $x = $targetPos->getX();
-           $y = $targetPos->getY();
-           $z = $targetPos->getZ();
 
-           $maxHeight = 4.0;
-           $step = 0.5;
-           $offset = 1.5;
+        $targetPos = $event->getEntity()->getPosition();
+        $world = $player->getWorld();
 
-           for ($i = 0; $i <= $maxHeight; $i += $step) {
-               $currentY = $y + $i;
+        $isOnGround = $event->getEntity()->isOnGround();
+        $soundName = $isOnGround
+            ? ($fallDistance > self::HEAVY_SMASH_THRESHOLD ? "mace.heavy_smash_ground" : "mace.smash_ground")
+            : "mace.smash_air";
 
-               $positions = [
-                   new Vector3($x + $offset, $currentY, $z),
-                   new Vector3($x - $offset, $currentY, $z),
-                   new Vector3($x, $currentY, $z + $offset),
-                   new Vector3($x, $currentY, $z - $offset),
-               ];
+        $this->playSoundToNearbyPlayers($world, $targetPos, $soundName);
 
-               foreach ($positions as $particlePos) {
-                   $world->addParticle(
-                       $particlePos,
-                       new BlockBreakParticle(StringToItemParser::getInstance()->parse($block)->getBlock())
-                   );
-               }
-           }
+        if ($isOnGround) {
+            $this->spawnSmashParticles($world, $targetPos);
+        }
 
-           $knockback = new Vector3($player->getMotion()->x, $player->getMotion()->y, $player->getMotion()->z);
+        $this->knockbackNearbyEntities($world, $player, $event->getEntity(), $fallDistance);
 
-           $knockback->x /= 2.0;
-           $knockback->y /= 2.0;
-           $knockback->z /= 2.0;
+        $knockback = new Vector3(
+            $player->getMotion()->x / 2.0,
+            0.01
+            $player->getMotion()->z / 2.0
+        );
 
-           $knockback->x -= ($player->getPosition()->x - $player->getPosition()->x) * 0.3;
-           if (($enchant = $item->getEnchantment(StringToEnchantmentParser::getInstance()->parse("wind_burst"))) !== null) {
-               switch ($enchant->getLevel()) {
-                   case 1:
-                       $knockback->y += 1.2;
-                       break;
-                       case 2:
-                           $knockback->y += 2.0;
-                           break;
-                           case 3:
-                               $knockback->y += 3.1;
-                               break;
-               }
-           } else {
-               $knockback->y += 0.7;
-           }
+        $windBurstEnchant = $item->getEnchantment(StringToEnchantmentParser::getInstance()->parse("wind_burst"));
+        if ($windBurstEnchant !== null) {
+            switch ($windBurstEnchant->getLevel()) {
+                case 1:
+                    $knockback->y = 1.2;
+                    break;
+                case 2:
+                    $knockback->y = 2.0;
+                    break;
+                case 3:
+                    $knockback->y = 3.1;
+                    break;
+            }
+        }
 
-           $knockback->z -= ($player->getPosition()->z - $player->getPosition()->z) * 0.3;
+        $player->setMotion($knockback);
+    }
 
-           $player->setMotion($knockback);
+    private function calculateBonusDamage(float $fallDistance): float
+    {
 
-           $nearbyEntities = $world->getNearbyEntities(new AxisAlignedBB($x - 20, $y - 20, $z - 20, $x + 20, $y + 20, $z + 20));
-           foreach ($nearbyEntities as $entity) {
-               if ($entity instanceof Player) {
-                   $entity->getNetworkSession()->sendDataPacket(PlaySoundPacket::create(
-                       "mace.heavy_smash_ground",
-                       $x,
-                       $y,
-                       $z,
-                       1.0,
-                       1.0
-                   ));
-               }
-           }
-       }
-   }
+        if ($fallDistance <= 3.0) {
+            return 4.0 * $fallDistance;
+        } elseif ($fallDistance <= 8.0) {
+            return 12.0 + 2.0 * ($fallDistance - 3.0);
+        } else {
+            return 22.0 + $fallDistance - 8.0;
+        }
+    }
+
+    private function knockbackNearbyEntities(\pocketmine\world\World $world, Player $player, \pocketmine\entity\Entity $attacked, float $fallDistance): void
+    {
+        $targetPos = $attacked->getPosition();
+        $range = self::KNOCKBACK_RANGE;
+
+        $boundingBox = new AxisAlignedBB(
+            $targetPos->x - $range,
+            $targetPos->y - $range,
+            $targetPos->z - $range,
+            $targetPos->x + $range,
+            $targetPos->y + $range,
+            $targetPos->z + $range
+        );
+
+        $nearbyEntities = $world->getNearbyEntities($boundingBox);
+
+        foreach ($nearbyEntities as $entity) {
+            if ($entity === $player || $entity === $attacked) {
+                continue;
+            }
+
+            if (!($entity instanceof \pocketmine\entity\Living)) {
+                continue;
+            }
+
+            $distance = $entity->getPosition()->distance($targetPos);
+            if ($distance > $range) {
+                continue;
+            }
+
+            $knockbackStrength = ($range - $distance) * self::KNOCKBACK_POWER;
+            if ($fallDistance > self::HEAVY_SMASH_THRESHOLD) {
+                $knockbackStrength *= 2;
+            }
+
+            $direction = $entity->getPosition()->subtract(
+                $targetPos->x,
+                $targetPos->y,
+                $targetPos->z
+            )->normalize();
+
+            $entity->setMotion(new Vector3(
+                $direction->x * $knockbackStrength,
+                0.7,
+                $direction->z * $knockbackStrength
+            ));
+        }
+    }
+
+    private function spawnSmashParticles(\pocketmine\world\World $world, Vector3 $pos): void
+    {
+        $blockUnder = $world->getBlock($pos->subtract(0, 1, 0));
+        $block = ($blockUnder instanceof Air) ? "grass" : $blockUnder->getName();
+
+        $maxHeight = 4.0;
+        $step = 0.5;
+        $offset = 1.5;
+
+        for ($i = 0; $i <= $maxHeight; $i += $step) {
+            $currentY = $pos->y + $i;
+
+            $positions = [
+                new Vector3($pos->x + $offset, $currentY, $pos->z),
+                new Vector3($pos->x - $offset, $currentY, $pos->z),
+                new Vector3($pos->x, $currentY, $pos->z + $offset),
+                new Vector3($pos->x, $currentY, $pos->z - $offset),
+            ];
+
+            foreach ($positions as $particlePos) {
+                $world->addParticle(
+                    $particlePos,
+                    new BlockBreakParticle(StringToItemParser::getInstance()->parse($block)->getBlock())
+                );
+            }
+        }
+    }
+
+    private function playSoundToNearbyPlayers(\pocketmine\world\World $world, Vector3 $pos, string $soundName): void
+    {
+        $nearbyEntities = $world->getNearbyEntities(new AxisAlignedBB(
+            $pos->x - 20,
+            $pos->y - 20,
+            $pos->z - 20,
+            $pos->x + 20,
+            $pos->y + 20,
+            $pos->z + 20
+        ));
+
+        foreach ($nearbyEntities as $entity) {
+            if ($entity instanceof Player) {
+                $entity->getNetworkSession()->sendDataPacket(PlaySoundPacket::create(
+                    $soundName,
+                    $pos->x,
+                    $pos->y,
+                    $pos->z,
+                    1.0,
+                    1.0
+                ));
+            }
+        }
+    }
 }
